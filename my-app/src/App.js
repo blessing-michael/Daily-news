7804b7d3e19bd5ba146b42a068effd6bafd5885f
blessing-michael <blessingmike334@gmail.com>
2023-04-06 09:51:50 -0700

    setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Nav from './Component/Nav'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+        <Nav/>
+    </div>
   )
 }
 
---
+++ my-app/src/Component/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+function Nav() {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Nav
