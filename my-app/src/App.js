73ba8dc807393c5f33193742c75d4efaf02b793a
blessing-michael <blessingmike334@gmail.com>
2023-04-07 11:52:31 -0700

    path create

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Nav from './Component/Nav'
+import Nav from './Pages/Nav'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from "./Component/Home"
 import Business from "./Component/Business"
@@ -11,16 +11,20 @@ import SignUp from "./Component/SignUp"
 import ForgotPassword from './Component/ForgotPassword'
 import Footer from './Component/Footer'
 
+
 function App() {
+    // const location=useLocation()
   return (
     <div>
-        <Nav/>
+      
         <Router>
+        <Nav/>
             <Routes>
+              
                 <Route path='/' element={<Home/>}/>
                 <Route path='/business' element={<Business/>}/>
                 <Route path='/science' element={<Science/>}/>
-                <Route path='/Tech' element={<Tech/>}/>
+                <Route path='/tech' element={<Tech/>}/>
                 <Route path='/sport' element={<Sport/>}/>
                 <Route path='/sign-in' element={<SignIn/>}/>
                 <Route path='/sign-up' element={<SignUp/>}/>
---
+++ my-app/src/Pages/Nav.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+
+
+
+function Nav() {
+  const navigate= useNavigate()
+  // const location= useLocation()
+  // // console.log(location)
+  
+  // function pathlocation(route){
+  //   if(route===location.pathname){
+  //     return true
+  //   }
+  // }
+ 
+  return (
+  
+    <div className='nav'>
+        <div className="img-div">
+            <div className="logo-div" onClick={()=>navigate("/")}>
+            <h1>Daily<span className='news'>News</span></h1>
+            
+
+            </div>
+            <div className="signIn-div">
+            <h3><span className='font-icon'><FontAwesomeIcon icon="fa-solid fa-user" /></span>sign in</h3>
+          
+            </div>
+           
+          
+        </div>
+        {/* <Link to="/">Home</Link>  */}
+        <nav className='list-Div'>
+            <ul>
+               <li onClick={()=>navigate("/home")}>Home</li>
+               <li onClick={()=>navigate("/business")}>Business</li>
+               <li onClick={()=>navigate("/science")}>Science</li>
+               <li onClick={()=>navigate
+              ("/tech")}>Tech</li>
+               <li onClick={()=>navigate
+              ("/sport")}>Sport</li>
+            </ul>
+        </nav>
+    </div>
+  
+  )
+}
+
+export default Nav
