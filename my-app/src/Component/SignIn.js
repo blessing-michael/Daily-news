2c06ac6243b022d1999eb9b4a366ff623465619e
blessing-michael <blessingmike334@gmail.com>
2023-04-08 12:56:16 -0700

    sign-in auth completed

--- my-app/src/Component/SignIn.js
+++ my-app/src/Component/SignIn.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { signInWithEmailAndPassword,getAuth } from 'firebase/auth';
 
 function SignIn() {
+    const [info, setInfo]= useState({
+        email:"",
+        password:"",
+    })
+    const {email,password}=info;
     const navigate= useNavigate()
+
+    function onChange(e){
+        // console.log(e.target.value)
+        setInfo((prevState)=>({
+            ...prevState, [e.target.id]:e.target.value
+
+        }))
+
+    }
+
+    async function onSubmit(e){
+        e.preventDefault()
+        try {
+            const auth= getAuth()
+            const userCredential= await signInWithEmailAndPassword(auth,email,password)
+            if(userCredential){
+                navigate("/")
+            }
+
+            
+        } catch (error) {
+            toast.error("wrong user credential")
+            
+        }
+
+    }
   return (
     <div className='signin-div'>signIn
         <div className="signin-Container">
-            <form action="">
+            <form action="" onSubmit={onSubmit}>
         <div className="logo-Signin">
             <h1 style={{color:"white"}}>Daily<span className='news'>News</span></h1>
             </div>
@@ -16,12 +49,12 @@ function SignIn() {
 
             </div>
             <div className="email">
-            <input type="email" placeholder='Email address'/>
+            <input type="email" placeholder='Email address' value={email} id="email" onChange={onChange}/>
            
 
             </div>
             <div className="password">
-            <input type="password" name="password" id="password" placeholder='Password' />
+            <input type="password" name="password" id="password" placeholder='Password' value={password} onChange={onChange} />
             </div>
 
             
