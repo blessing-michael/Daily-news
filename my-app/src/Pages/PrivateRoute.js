d5b5137303744120e5f182ca1b0888fa8b8dc3ee
blessing-michael <blessingmike334@gmail.com>
2023-04-09 18:13:14 -0700

    cretae spinner component

--- my-app/src/Pages/PrivateRoute.js
+++ my-app/src/Pages/PrivateRoute.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Outlet, Navigate} from 'react-router-dom';
 import  {useAuthStatus} from '../hooks/useAuthStatus';
+import Spinner from './Spinner';
 
 function PrivateRoute() {
     // const loggedIn =false;
     const {loggedIn, checkingStatus}= useAuthStatus()
     if(checkingStatus){
-        return <h1>loading....</h1>
+        return <div className="div">
+            <Spinner/>
+            <h1>hey................</h1>
+
+        </div>  
+
     }
 
   return loggedIn? <Outlet/> :<Navigate to= "/sign-in"/>
---
+++ my-app/src/Pages/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import spinner from "../assets/svg/spinner.svg"
+
+function Spinner() {
+  return (
+    <div style={{backgroundColor:"black", opacity:"0.7"}}>Spinner
+        <img src={spinner} alt="loading..."  style={{height:"150px" ,width:"150px"}}/>
+    </div>
+  )
+}
+
+export default Spinner
