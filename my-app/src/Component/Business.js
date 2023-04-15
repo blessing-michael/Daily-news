d2aa336c41c8335d0269584ff43ca0bdfc6944d9
blessing-michael <blessingmike334@gmail.com>
2023-04-15 14:22:12 -0700

    footer ,refresh news functionality files

--- my-app/src/Component/Business.js
+++ my-app/src/Component/Business.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import Footer from "./Footer";
 // import spinner from "../assets/svg/spinner.svg"
 
 // import uniqid from "uniqid";
@@ -7,55 +9,94 @@ function Business() {
   const [loading, setLoading] = useState(false);
   const [results, setResults] = useState([]);
 
-  
   const fetchnews = async () => {
-    let mixgen = ["205f3fe", "c4cd3", "2b40b5", "846d65", "062c9b", "5"];
-    let secone = "20";
-    // let sectwo = "bea"
-    let sectwo = "85d";
+    try {
+      let mixgen = ["205f3fe", "c4cd3", "2b40b5", "846d65", "062c9b", "5"];
+      let secone = "20";
+      // let sectwo = "bea"
+      let sectwo = "85d";
 
-    let secretGif = sectwo.split("").reverse().join("");
-    console.log(secretGif);
+      let secretGif = sectwo.split("").reverse().join("");
+      console.log(secretGif);
 
-    let stepOne = mixgen.join("").split("").join("");
-    // console.log(stepOne)
+      let stepOne = mixgen.join("").split("").join("");
+      // console.log(stepOne)
 
-    let stepTwo = stepOne.split(secone);
-    console.log(stepTwo, "stepTWo.....");
+      let stepTwo = stepOne.split(secone);
+      console.log(stepTwo, "stepTWo.....");
 
-    let stepThree = stepTwo[1] + secretGif;
-    console.log(stepThree);
+      let stepThree = stepTwo[1] + secretGif;
+      console.log(stepThree);
 
-    setLoading(true);
-    const response = await fetch(
-      ` https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${stepThree}`
-    );
+      setLoading(true);
+      const response = await fetch(
+        ` https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${stepThree}`
+      );
 
-    const dailynews = await response.json();
+      const dailynews = await response.json();
 
-    console.log(dailynews, "new api..............");
-    setResults(dailynews.articles);
-    // console.log(dailynews.results[0].category,"res")
-    console.log(dailynews.articles, "res");
+      console.log(dailynews, "new api..............");
+      setResults(dailynews.articles);
+      // console.log(dailynews.results[0].category,"res")
+      console.log(dailynews.articles, "res");
+    } catch (error) {
+      toast.error("something went wrong, please try again");
+    }
   };
   // console.log(news, "..................")
 
   useEffect(() => {
     fetchnews();
-   
   }, []);
+  function Fetchagain() {
+    try {
+      fetchnews();
+    } catch (error) {
+      toast.error("something went wrong while fetching news");
+    }
 
-//   if(loading===false){
-//     return <div className="spinner-container">
-//     {/* <h4>loading.....</h4> */}
-//     <img src={spinner} alt="loading" className="spinner" />
-//     {console.log(spinner, "showing spinner")}
-    
-//     </div>
+    // setResults(dailynews.article)
+  }
+  if (results.length === 0) {
+    console.log(results.length, "reselts");
+    return (
+      <main>
+        <div
+          className="no-tours"
+          style={{ position: "absolute", left: "45%", top: "50%" }}
+        >
+          <h1 style={{ color: "black" }}>No News Left</h1>
+          <button
+            onClick={Fetchagain}
+            className="refresh-btn"
+            style={{
+              textAlign: "center",
+              marginTop: "20px",
+              marginLeft: "25%",
+              padding: "5px 25px",
+              backgroundColor: "skyblue",
+              color: "white",
+              border: "none",
+              borderRadius: "2px",
+              cursor: "pointer",
+            }}
+          >
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
 
-//   }
+  //   if(loading===false){
+  //     return <div className="spinner-container">
+  //     {/* <h4>loading.....</h4> */}
+  //     <img src={spinner} alt="loading" className="spinner" />
+  //     {console.log(spinner, "showing spinner")}
 
+  //     </div>
 
+  //   }
 
   function Notinterested(id) {
     const newinfo = results.filter((result) => result.title !== id);
@@ -65,6 +106,19 @@ function Business() {
 
   return (
     <div className="newsdata">
+      <div
+        className="head-title"
+        style={{
+          backgroundColor: "red",
+          position: "absolute",
+          left: "0",
+          width: "100%",
+          borderBottom: "1px solid black",
+          padding: "6px 0px",
+        }}
+      >
+        <h1 style={{ color: "white", marginLeft: "40px" }}>BUSINESS</h1>
+      </div>
       {results.map((res) => {
         const image_url = res.urlToImage;
         //   const id=uniqid()
@@ -121,7 +175,7 @@ function Business() {
         );
       })}
 
-      <h1 className="app">App</h1>
+      <Footer />
     </div>
   );
 }
--- my-app/src/Component/Footer.js
+++ my-app/src/Component/Footer.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div style={{backgroundColor:"black", height:"250px", marginTop:"50px"}}>
+       <div className="footer" style={{ marginTop:"50px",width:"350px", position:"absolute",left:"38%"}} >
+      <p style={{color:"white", textAlign:"center",}}> &copy; {new Date().getFullYear()} Blessing Michael. All Rights Reserved.</p>
+      </div>
+    </div>
   )
 }
 
